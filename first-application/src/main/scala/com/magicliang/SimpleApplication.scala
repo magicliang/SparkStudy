05d40c45f1e1178d26c2afb82c2a7e91965313d5
liangchuan <liangchuan@didichuxing.com>
2018-01-29 14:40:54 +0800

    增加 standalone deploy mode 模式

--- first-application/src/main/scala/com/magicliang/SimpleApplication.scala
+++ first-application/src/main/scala/com/magicliang/SimpleApplication.scala
@@ -14,7 +14,7 @@ object SimpleApplication {
   // 在 VM options 里增加 -Dspark.master=local 即可指定应用程序在本地单线程启动。
   // 暂时不知道为什么用 ide 启动只支持 local[k] 模式而不支持提交任务到 master url 的模式
   // https://spark.apache.org/docs/latest/submitting-applications.html spark 的几种启动模式参数，也可以在 submit 里指定
-  //
+  // Spark 模式的总结：https://www.jianshu.com/p/65a3476757a5
   def main(args: Array[String]): Unit = {
     val logFile = "/Users/magicliang/Desktop/Programming/tools/spark-2.2.1-bin-hadoop2.7/README.md"
     val spark = SparkSession.builder.appName("SimpleApplication").getOrCreate()
